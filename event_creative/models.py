2b01e5250ec3285881da3f652ebb7198a122a2df
ks <seleznev.nvkz@gmail.com>
2015-01-03 18:07:36 +0700

    new model

--- event_creative/models.py
+++ event_creative/models.py
@@ -46,3 +46,25 @@ class Report(models.Model):
 
     def get_absolute_url(self):
         return reverse('report', args=[str(self.pk), ])
+
+
+class Services(models.Model):
+    """
+    Услуги
+    """
+    main_title = models.CharField(max_length=32,  verbose_name=u'Основной Заголовок')
+    add_title = models.CharField(max_length=128,  verbose_name=u'Подробный заголовок')
+    body = RedactorField(verbose_name=u'Текст статьи')
+    create_date = models.DateTimeField(verbose_name=u'Дата создания')
+    active = models.BooleanField(default=True, verbose_name=u'Показывать')
+    image = models.ImageField(verbose_name=u'Постер', blank=True, null=True)
+    update_date = models.DateTimeField(auto_now=True, verbose_name=u'Дата изменения')
+
+    class Meta:
+        verbose_name = verbose_name_plural = u'Услуги'
+
+    def __unicode__(self):
+        return self.main_title
+
+    def get_absolute_url(self):
+        return reverse('service', args=[str(self.pk), ])
