1bf21964ccf403ed38cdd314f5c0c18817874f73
Seleznev-nvkz <seleznev.nvkz@gmail.com>
2015-02-09 12:48:04 +0700

    infinite cache

--- event_creative/urls.py
+++ event_creative/urls.py
@@ -2,15 +2,13 @@ from django.conf.urls import patterns, url
 from django.views.decorators.cache import cache_page
 from views import *
 
-n = 60*60*24
-
 urlpatterns = patterns(
     'event_creative.urls',
-    url(r'^$', cache_page(n)(Index.as_view()), name='main_page'),
-    url(r'^articles/$', cache_page(n)(ArticleList.as_view()), name='article_list'),
+    url(r'^$', cache_page(0)(Index.as_view()), name='main_page'),
+    url(r'^articles/$', cache_page(0)(ArticleList.as_view()), name='article_list'),
     # url(r'^contacts/$', Contacts.as_view(), name='contacts'),
-    url(r'^article/(?P<pk>[0-9]{1,3})/$', cache_page(n)(ArticleDetail.as_view()), name='article'),
-    url(r'^services/$', cache_page(n)(ServicesList.as_view()), name='service_list'),
-    url(r'^reports/$', cache_page(n)(ReportList.as_view()), name='report_list'),
-    url(r'^report/(?P<pk>[0-9]{1,3})/$', cache_page(n)(ReportDetail.as_view()), name='report'),
+    url(r'^article/(?P<pk>[0-9]{1,3})/$', cache_page(0)(ArticleDetail.as_view()), name='article'),
+    url(r'^services/$', cache_page(0)(ServicesList.as_view()), name='service_list'),
+    url(r'^reports/$', cache_page(0)(ReportList.as_view()), name='report_list'),
+    url(r'^report/(?P<pk>[0-9]{1,3})/$', cache_page(0)(ReportDetail.as_view()), name='report'),
 )
